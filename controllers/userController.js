66d62151a22fceec57e0fc4d1b9bd9c84cb5a3ea
hasinirajasinghe <hasinichathu@gmail.com>
2022-06-10 21:37:19 -0400

    user shopping cart partially working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,16 @@
 const User = require('../models/user')
 
-// Fetches all the users 
-const getAllUsers = (req,res) => {
-    User.find({}, (err, users) => {
-        if (err) {
-            res.status(400).json(err)
-            return
-        }
-        res.json(users)
-    })
-}
-
-// Show sign up page 
-const showSignUpPage = (req,res) => {
-    res.render('sign_up_page')
-}
-
-// Creates new user 
-const createNewUser = (req,res) => {
-    User.create(req.body, (err) => {
-        if (err) {
-            res.status(400).json(err)
-            return
-        }
-        res.redirect('/log-in')
-    })
-}
-
-// Show log in page 
-const showLogInPage = (req,res) => {
-    res.render('log_in_page')
-}
-
-// Authenticate the user and redirects to homepage
-const logInUser = (req, res) => {
-    // TODO: Add authentication code
-    res.redirect('/')
-}
-
-// Fetches user by user ID
-const getUserById = (req,res) => {
+const viewCart = (req,res) => {
     User.findById(req.params.id, (err,user) => {
-        if (err) {
+        if(err) {
             res.status(400).json(err)
             return
         }
-        res.json(user)
+        res.render('shopping-cart-page', {user})
     })
 }
 
 const addToCart = (req,res) => {
-    console.log('made it')
     User.findById(req.params.id, (err,user) => {
         if(err) {
             res.status(400).json(err)
@@ -63,11 +23,6 @@ const addToCart = (req,res) => {
 }
 
 module.exports = {
-    getAllUsers,
-    showSignUpPage,
-    createNewUser,
-    showLogInPage,
-    logInUser,
-    getUserById, 
+    viewCart,
     addToCart 
 }
