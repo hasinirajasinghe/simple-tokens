3ee7e2abaa41d974b329c84bec0691f87147246e
hasinirajasinghe <hasinichathu@gmail.com>
2022-06-12 19:20:52 -0400

    added more css

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -3,27 +3,27 @@ function loadReviews() {
     let reviewContent = document.getElementById('review-content')
     reviewContent.replaceChildren()
     let reviewTable = document.createElement('table');
+    reviewTable.className = 'all-reviews-container'
     reviewContent.append(reviewTable)
-    // makes a request to the server to fetch all reviews. Essentially GET request. 
+    // makes a request to the server to fetch all reviews. Essentially a GET request. 
     fetch('/reviews/', { method: 'GET'})
     .then(res => res.json())
     .then(jsonData => {
         jsonData.forEach(review => {
             let reviewRow = document.createElement('tr');
+            reviewRow.className = 'review-row'
             let reviewName = document.createElement('td')
             reviewName.innerText = review.name
+            reviewName.className = 'review-column1'
             let reviewRating = document.createElement('td')
             reviewRating.innerText = review.rating
+            reviewRating.className = 'review-column2'
             let reviewText = document.createElement('td')
             reviewText.innerText = '"' + review.reviewText + '"'
+            reviewText.className = 'review-column3'
 
             reviewRow.append(reviewName, reviewRating, reviewText);
             reviewTable.append(reviewRow)
-            // let reviewDiv = document.createElement('div')
-            
-            // reviewDiv.className = 'user-review';
-            // reviewDiv.innerHTML = review.reviewText;
-            // reviewContent.append(reviewDiv);
         })
     })
     .catch(e => {alert('Failed to fetch reviews. Please try again!')})
